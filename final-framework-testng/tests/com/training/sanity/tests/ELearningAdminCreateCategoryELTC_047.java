9159ab4b6b1047b21e582983b9bbab1c877b5e57
MALLIGownivari <MALLIGownivari@DESKTOP-IM89DI8.in.ibm.com>
2019-10-31 13:42:58 +0530

    Medium level

---
+++ final-framework-testng/tests/com/training/sanity/tests/ELearningAdminCreateCategoryELTC_047.java
@@ -0,0 +1,65 @@
+package com.training.sanity.tests;
+
+import java.awt.AWTException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.training.pom.LoginPOMLearning;
+import com.training.utility.DriverFactory;
+import com.training.utility.DriverNames;
+
+public class ELearningAdminCreateCategoryELTC_047 {
+
+	public WebDriver driver;
+	private String baseUrl;
+	public LoginPOMLearning loginPOM;
+	private static Properties properties;
+	@BeforeClass
+	public static void setUpBeforeClass() throws IOException {
+		properties = new Properties();
+		FileInputStream inStream = new FileInputStream("./resources/others.properties");
+		properties.load(inStream);
+	}
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		driver = DriverFactory.getDriver(DriverNames.CHROME);
+		loginPOM = new LoginPOMLearning(driver); 
+		baseUrl = properties.getProperty("baseURL");
+		// open the browser 
+		driver.get(baseUrl);
+	}
+	
+	@AfterClass
+	public void tearDown() throws Exception {
+		Thread.sleep(1000);
+		driver.close();
+	}
+	
+	
+	@Test
+	public void validLoginTest() throws AWTException {
+		loginPOM.sendUserName("admin");
+		loginPOM.sendPassword("admin@123");
+		loginPOM.clickLoginBtn(); 
+		loginPOM.createCategory("BL","Blended Learning");
+		loginPOM.createCourse("testing", "tes", "BLENDED LEARNING","manzoor", "Selenium", "selenium.hq");
+		loginPOM.submitbutton();
+		
+		
+	}
+	
+	
+	
+	
+			
+	}
---
+++ final-framework-testng/tests/com/training/sanity/tests/ELearningStudentCommentELTC_046.java
@@ -0,0 +1,54 @@
+package com.training.sanity.tests;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.training.pom.LoginPOMLearning;
+import com.training.utility.DriverFactory;
+import com.training.utility.DriverNames;
+
+public class ELearningStudentCommentELTC_046 {
+	public WebDriver driver;
+	private String baseUrl;
+	public LoginPOMLearning loginPOM;
+	private static Properties properties;
+	@BeforeClass
+	public static void setUpBeforeClass() throws IOException {
+		properties = new Properties();
+		FileInputStream inStream = new FileInputStream("./resources/others.properties");
+		properties.load(inStream);
+	}
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+		driver = DriverFactory.getDriver(DriverNames.CHROME);
+		loginPOM = new LoginPOMLearning(driver); 
+		baseUrl = properties.getProperty("baseURL");
+		// open the browser 
+		driver.get(baseUrl);
+	}
+	
+	@AfterMethod
+	public void tearDown() throws Exception {
+		Thread.sleep(1000);
+		driver.close();
+	}
+	
+	
+	@Test
+	public void validLoginTest() throws InterruptedException {
+		loginPOM.sendUserName("admin");
+		loginPOM.sendPassword("admin@123");
+		loginPOM.clickLoginBtn(); 
+		loginPOM.courseCatelogAddComment("Testing", "Testing");
+		
+	}
+	
+}
